10857a3cb3e1ea113b445aedd8e4989105305420
aamerk2 <jawwadaamer@gmail.com>
2016-08-24 15:53:54 +1000

    added comments to the IStudent.java interface

--- ITC515_assignment2/src/datamanagement/IStudent.java
+++ ITC515_assignment2/src/datamanagement/IStudent.java
@@ -4,15 +4,15 @@ public interface IStudent {
 
     public Integer getID();//Method getId which returns integer value
 
-    public String getFirstName();
-    public void setFirstName(String firstName);
+    public String getFirstName();//Method getFirstName which returns String value
+    public void setFirstName(String firstName);//Method setFirstName which dosen't return any value but takes firstName as input parameter
 
-    public String getLastName();
-    public void setLastName(String lastName);
+    public String getLastName();//Method getLastName which returns String value
+    public void setLastName(String lastName);//Method setLastName which dosen't return any value but takes lastName as input parameter
 
-    public void addUnitRecord( IStudentUnitRecord record );
-    public IStudentUnitRecord getUnitRecord( String unitCode );
+    public void addUnitRecord( IStudentUnitRecord record );//Method addUnitRecord which returns integer value, takes record of type casted value IStudentUnitRecord as input parameter 
+    public IStudentUnitRecord getUnitRecord( String unitCode );//Method getUnitRecord which returns IStudentUnitRecord value takes unitCode as input value which is a string
 
-    public StudentUnitRecordList getUnitRecords();
+    public StudentUnitRecordList getUnitRecords();//Method getUnitRecords which returns  value of StudentUnitRecordList
 
 }
