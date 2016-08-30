f2cb2794e50a38ff438417dddd39ff952c272019
aamerk2 <jawwadaamer@gmail.com>
2016-08-30 16:09:25 +1000

    Code Allignment for all the classes
    For Static code review

--- ITC515_assignment2/src/datamanagement/IStudentLister.java
+++ ITC515_assignment2/src/datamanagement/IStudentLister.java
@@ -4,9 +4,9 @@ package datamanagement;
  * @author jtulip
  */
 
-public interface IStudentLister
-{
+public interface IStudentLister {
 
-    public void clearStudents();
-    public void addStudent(IStudent student);
+	public void clearStudents();
+
+	public void addStudent(IStudent student);
 }
